085d1b4a032006a547f796ecff66ed2277686431
Chad Wilson <cwilson66@mail.csuchico.edu>
2018-08-06 15:37:29 -0700

    Makes main content animation

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
     $("#open_menu").click(function() {
         $("#menu").css({"display":"flex"});
-        $("#component-main_content").css({
-            "opacity":"0.01",
-            "pointer-events":"none"
-        });
+        $("#component-main_content").css({"pointer-events":"none"});
+        if($("#component-main_content").hasClass("component-drop_down_menu_close_animation"))
+            {
+                $("#component-main_content").removeClass("component-drop_down_menu_close_animation");
+            }
+        $("#component-main_content").addClass("component-drop_down_menu_open_animation");
     });
     $("#close_menu").click(function() {
         $("#menu").css({"display":"none"});
-        $("#component-main_content").css({
-            "opacity":"1",
-            "pointer-events":"auto"
-        });
+        if($("#component-main_content").hasClass("component-drop_down_menu_open_animation"))
+            {
+                $("component-main_content").removeClass("component-drop_down_menu_open_animation");
+            }
+        $("#component-main_content").addClass("component-drop_down_menu_close_animation");
+        $("#component-main_content").css({"pointer-events":"auto"});
     });
     $("#green_email_one").click(function() {
         $("#green_email_one").addClass("component-footer_background_color__green_emails_when_clicked");
