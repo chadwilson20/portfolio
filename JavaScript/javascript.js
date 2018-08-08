bb66cf06a1b084cda6b0e9dc86fc10a6e14b9f7f
Chad Wilson <cwilson66@mail.csuchico.edu>
2018-08-07 19:20:26 -0700

    cleanes script page code and comments

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,3 +1,37 @@
+//This function is to copy the emails on each page to the clipboard and to style the link
+//once it is clicked when the emails are clicked.
+function DropDownMenu(id) {
+    if(id == "green_email_one") {
+        $("#green_email_one").addClass("component-footer_background_color__green_emails_when_clicked");
+    }
+    else if(id == "green_email_two") {
+        $("#green_email_two").addClass("component-footer_background_color__green_emails_when_clicked");
+    }
+    else if(id == "grey_email") {
+        $("#grey_email").addClass("component-footer_background_color__grey_email_when_clicked");
+    }
+    else if(id == "drop_down_menu_email") {
+        $("#drop_down_menu_email").addClass("component-drop_down_menu__email_when_clicked");
+    }
+    var range = document.createRange();
+    var selection = window.getSelection();
+    if(id == "green_email_one") {
+        range.selectNodeContents(document.getElementById('green_email_one'));
+    }
+    else if(id == "green_email_two") {
+        range.selectNodeContents(document.getElementById('green_email_two'));
+    }
+    else if(id == "grey_email") {
+        range.selectNodeContents(document.getElementById('grey_email'));
+    }
+    else if(id == "drop_down_menu_email") {
+        range.selectNodeContents(document.getElementById('drop_down_menu_email'));
+    }
+    selection.addRange(range);
+    document.execCommand('copy');
+    selection.removeAllRanges();
+}
+
 $(document).ready(function() {
     //This function is to display the drop-down menu when the menu button is clicked
     $("#open_menu").click(function() {
@@ -6,15 +40,15 @@ $(document).ready(function() {
             "display":"flex"
         });
         if($("#menu").hasClass("component-drop_down_menu_hide_animation"))
-            {
-                $("#menu").removeClass("component-drop_down_menu_hide_animation");
-            }
+        {
+            $("#menu").removeClass("component-drop_down_menu_hide_animation");
+        }
         $("#menu").addClass("component-drop_down_menu_show_animation");
         $("#component-main_content").css({"pointer-events":"none"});
         if($("#component-main_content").hasClass("component-drop_down_menu_close_animation"))
-            {
-                $("#component-main_content").removeClass("component-drop_down_menu_close_animation");
-            }
+        {
+            $("#component-main_content").removeClass("component-drop_down_menu_close_animation");
+        }
         $("#component-main_content").addClass("component-drop_down_menu_open_animation");
     });
     //This function is to close the drop-down menu when the exit button is clicked
@@ -24,55 +58,15 @@ $(document).ready(function() {
             "display":"none"
         });
         if($("#menu").hasClass("component-drop_down_menu_show_animation"))
-            {
-                $("#menu").removeClass("component-drop_down_menu_show_animation");
-            }
+        {
+            $("#menu").removeClass("component-drop_down_menu_show_animation");
+        }
         $("#menu").addClass("component-drop_down_menu_hide_animation");
         if($("#component-main_content").hasClass("component-drop_down_menu_open_animation"))
-            {
-                $("component-main_content").removeClass("component-drop_down_menu_open_animation");
-            }
+        {
+            $("component-main_content").removeClass("component-drop_down_menu_open_animation");
+        }
         $("#component-main_content").addClass("component-drop_down_menu_close_animation");
         $("#component-main_content").css({"pointer-events":"auto"});
     });
-    $("#green_email_one").click(function() {
-        $("#green_email_one").addClass("component-footer_background_color__green_emails_when_clicked");
-        var range = document.createRange();
-        var selection = window.getSelection();
-        range.selectNodeContents(document.getElementById('green_email_one'));
-        selection.removeAllRanges();
-        selection.addRange(range);
-        document.execCommand('copy');
-        selection.removeAllRanges();
-    });
-    $("#green_email_two").click(function() {
-        $("#green_email_two").addClass("component-footer_background_color__green_emails_when_clicked");
-        var range = document.createRange();
-        var selection = window.getSelection();
-        range.selectNodeContents(document.getElementById('green_email_two'));
-        selection.removeAllRanges();
-        selection.addRange(range);
-        document.execCommand('copy');
-        selection.removeAllRanges();
-    })
-    $("#grey_email").click(function() {
-        $("#grey_email").addClass("component-footer_background_color__grey_email_when_clicked")
-        var range = document.createRange();
-        var selection = window.getSelection();
-        range.selectNodeContents(document.getElementById('grey_email'));
-        selection.removeAllRanges();
-        selection.addRange(range);
-        document.execCommand('copy');
-        selection.removeAllRanges();
-    })
-    $("#drop_down_menu_email").click(function() {
-        $("#drop_down_menu_email").addClass("component-drop_down_menu__email_when_clicked");
-        var range = document.createRange();
-        var selection = window.getSelection();
-        range.selectNodeContents(document.getElementById('drop_down_menu_email'));
-        selection.removeAllRanges();
-        selection.addRange(range);
-        document.execCommand('copy');
-        selection.removeAllRanges();
-    })
 });
